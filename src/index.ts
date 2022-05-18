23f541a2f44bb162e74c4d132e3b643e77e2ac08
theluposki <theluposki@gmail.com>
2022-05-18 03:25:01 -0300

    adding authentication

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,12 @@ import { app } from "./app";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`[ APP ] Running in http://localhost:${PORT}`));
+const server = app.listen(PORT, () => console.log(`[ APP ] Running in http://localhost:${PORT} pid: ${process.pid}`));
+
+process.on("SIGTERM", () => {
+    console.log("\n\n[ APP ][ Stoped ] \n\n", new Intl.DateTimeFormat('pt-br', { dateStyle: 'full', timeStyle: 'long' }).format(Date.now()))
+    server.close((error) => {
+        console.log("\n\n exit")
+        process.exit()
+    })
+})
